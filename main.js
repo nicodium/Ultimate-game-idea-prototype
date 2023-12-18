85747c51cc19df6da35b840aa6cec1c5e23a80a2
nicodium <123805115+nicodium@users.noreply.github.com>
2023-12-18 15:14:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function buy1(){
     var buy1Cost = Math.floor(10 * Math.pow(1.1,skimdata));     
     if(science >= buy1Cost){                                   
         science = science + 1;                                   
-    	cience = science - buy1Cost;                          
+    	science = science - buy1Cost;                          
         document.getElementById('buy1').innerHTML = buy1;  
         document.getElementById('science').innerHTML = science;  
     };
