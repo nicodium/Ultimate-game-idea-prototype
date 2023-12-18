7a981cc83286206d802127250fb5160084a7db72
nicodium <123805115+nicodium@users.noreply.github.com>
2023-12-18 15:05:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ function sciclick(number){
     science = science + number;
     document.getElementById("science").innerHTML = science;
 };
+var skimdata = 0;
+function buy1(){
+    var buy1Cost = Math.floor(10 * Math.pow(1.1,skimdata));     
+    if(science >= buy1Cost){                                   
+        science = science + 1;                                   
+    	cience = science - buy1Cost;                          
+        document.getElementById('buy1').innerHTML = buy1;  
+        document.getElementById('science').innerHTML = science;  
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,skimdata));       
+    document.getElementById('buy1Cost').innerHTML = nextCost;  
+
+window.setInterval(function(){
+	
+	sciclick(numer);
+	
+}, 1000);
+
